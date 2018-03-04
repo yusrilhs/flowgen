7103aa795e7182e36ca289e6447c426e63c47ee4
Yusril Herlian Syah <yusrilhsyah@gmail.com>
2018-03-04 23:26:30 +0700

    Add test.js

---
+++ test.js
@@ -0,0 +1,26 @@
+'use strict'
+const NdMail = require('.')
+    , argv = require('yargs').argv
+
+let ndmail = new NdMail({
+  imap: {
+    user: argv.email,
+    password: argv.password,
+    tls: true,
+    port: 993,
+    host: 'imap.gmail.com',
+    inbox: 'INBOX'
+  }
+})
+
+ndmail.on('imap_error', function(err) {
+  console.log(err)
+})
+
+ndmail.on('mail', function(msg) {
+  console.log(msg)
+})
+
+ndmail.connect(function() {
+  ndmail.fetchMailFrom(1)
+})
