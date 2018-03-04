b302ea2be9e87d977bc60f2eeb3242c7bca03132
Yusril Herlian Syah <yusrilhsyah@gmail.com>
2018-03-05 00:59:37 +0700

    Add sendMail test

--- test.js
+++ test.js
@@ -10,6 +10,15 @@ let ndmail = new NdMail({
     port: 993,
     host: 'imap.gmail.com',
     inbox: 'INBOX'
+  },
+  smtp: {
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, // use SSL
+    auth: {
+      user: argv.email,
+      pass: argv.password
+    }
   }
 })
 
@@ -27,4 +36,14 @@ ndmail.on('mail', function(msg) {
 
 ndmail.connect(function() {
   ndmail.fetchMailFrom(1)
+
+  if (argv.send) {
+    ndmail.sendMail({
+      from: argv.email,
+      to: argv.send,
+      subject: 'Test NdMail',
+      html: '<h1>Hello this is a mail from NdMail</h1>'
+    })
+  }
+  
 })
